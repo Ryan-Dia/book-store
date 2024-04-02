4739fc0c79f3a3b5d5b3c386d26938e8b2fa2929
ryan-dia <cjfdnjs158@naver.com>
2024-04-02 11:22:15 +0900

    feat:  MemberRepository 추가

---
+++ src/main/java/bookstore/shop/MemberRepository.java
@@ -0,0 +1,12 @@
+package bookstore.shop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+}
