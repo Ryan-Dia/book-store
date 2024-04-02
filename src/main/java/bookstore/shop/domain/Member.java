477f9033b93975617656b819c3e655068e20186b
ryan-dia <cjfdnjs158@naver.com>
2024-04-02 12:21:20 +0900

    fix: 오타 수정

--- src/main/java/bookstore/shop/domain/Member.java
+++ src/main/java/bookstore/shop/domain/Member.java
@@ -18,7 +18,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String name;
 
     @Embedded // 내장 타입을 포함 했다.
     private Address address;
