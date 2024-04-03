c7fca618d3d45fc30d3f1012d3b6a10dbce66241
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2024-04-04 01:09:39 +0900

    feat: 상품 도메인 개발 (#2)
    * feat: 상품 엔티티 추가
    
    - 비즈니스 로직 추가
    
    * feat: ItemRepository 구현
    
    * feat: ItemService 구현

--- src/main/java/bookstore/shop/domain/Item/Item.java
+++ src/main/java/bookstore/shop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package bookstore.shop.domain.Item;
 
 import bookstore.shop.domain.Category;
+import bookstore.shop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categoryies = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 객체지향적으로 생각해보면 데이터를 가지고 있는 쪽에 비즈니스 메서드가 있는게 응집력이 있어서 좋다.
+    // stock을 변경할 때 setter를 가지고 바깥에서 계산하는게 아니라 이 안에서 비즈니스 로직으로 변경한다.
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more sotck");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/bookstore/shop/service/ItemService.java
@@ -0,0 +1,30 @@
+package bookstore.shop.service;
+
+import bookstore.shop.domain.Item.Item;
+import bookstore.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // 메서드에 가까운게 우선권을 가짐
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
