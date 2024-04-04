a5309754b38f8a123e0f59f5389cabd1ba6eef63
ryan-dia <cjfdnjs158@naver.com>
2024-04-05 02:07:19 +0900

    feat: findAll 메서드 구현
    팅# 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)

--- src/main/java/bookstore/shop/repository/OrderRepository.java
+++ src/main/java/bookstore/shop/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package bookstore.shop.repository;
 
+import bookstore.shop.domain.QMember;
 import bookstore.shop.domain.order.Order;
+import bookstore.shop.domain.order.OrderStatus;
+import bookstore.shop.domain.order.QOrder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -21,5 +27,44 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll1(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                //.setFirstResult(100) // 이건 페이징할 때 사용
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if(!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if(statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
+    }
+
 }
