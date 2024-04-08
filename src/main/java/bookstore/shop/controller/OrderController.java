9b3648e7f8ac4ab1ab28a250c5b31d523a465347
ryan-dia <cjfdnjs158@naver.com>
2024-04-09 04:02:31 +0900

    feat: 주문 목록 검색,취소 기능 구현

--- src/main/java/bookstore/shop/controller/OrderController.java
+++ src/main/java/bookstore/shop/controller/OrderController.java
@@ -2,15 +2,15 @@ package bookstore.shop.controller;
 
 import bookstore.shop.domain.Item.Item;
 import bookstore.shop.domain.Member;
+import bookstore.shop.domain.order.Order;
+import bookstore.shop.repository.OrderSearch;
 import bookstore.shop.service.ItemService;
 import bookstore.shop.service.MemberService;
 import bookstore.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +41,23 @@ public class OrderController {
 
         // 수정해서 멀티로 가능 일단 여기서는 하나의 상품만
         orderService.order(memberId, itemId, count);
+
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        //model.addAttribute("orderSearch", orderSearch); ModelAttribute 세팅 덕분에 해당 코드를 안 쳐도 적용 되어있음
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+
         return "redirect:/orders";
     }
 }
