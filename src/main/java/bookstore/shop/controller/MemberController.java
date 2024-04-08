3924cd3247c5d8f6d19eeb4f50a041f852789eee
ryan-dia <cjfdnjs158@naver.com>
2024-04-08 21:23:41 +0900

    feat: 회원 목록 조회 구현

--- src/main/java/bookstore/shop/controller/MemberController.java
+++ src/main/java/bookstore/shop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -26,7 +28,7 @@ public class MemberController {
     @PostMapping("/member/new")
     public String create(@Valid MemberForm form, BindingResult result) {
 
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             return "members/createMemberForm";
         }
 
@@ -38,5 +40,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
