15c271e46637c2f2ec76ea4d41da54fe6b2133e5
ryan-dia <cjfdnjs158@naver.com>
2024-04-09 03:20:25 +0900

    feat: 주문 목록 기능 구현

---
+++ src/main/java/bookstore/shop/controller/OrderController.java
@@ -0,0 +1,46 @@
+package bookstore.shop.controller;
+
+import bookstore.shop.domain.Item.Item;
+import bookstore.shop.domain.Member;
+import bookstore.shop.service.ItemService;
+import bookstore.shop.service.MemberService;
+import bookstore.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        // 수정해서 멀티로 가능 일단 여기서는 하나의 상품만
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
