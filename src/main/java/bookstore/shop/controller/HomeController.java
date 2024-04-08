5d2826eb7d1ffbefca722e530ca9b4bb240816c5
ryan-dia <cjfdnjs158@naver.com>
2024-04-08 16:35:28 +0900

    feat: html 기본 세팅

--- src/main/java/bookstore/shop/controller/HomeController.java
+++ src/main/java/bookstore/shop/controller/HomeController.java
@@ -1,2 +1,13 @@
-package bookstore.shop.controller;public class HomeController {
+package bookstore.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
 }
